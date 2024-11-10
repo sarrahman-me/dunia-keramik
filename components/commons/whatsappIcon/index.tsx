fc78125fd7130ba17976fc1aeb4c1f7343696ac2
muhammad nur rahman <138513675+sarrahman-me@users.noreply.github.com>
2024-11-11 06:46:38 +0700

    :hammer: fix whatsapp icon posisition

--- components/commons/whatsappIcon/index.tsx
+++ components/commons/whatsappIcon/index.tsx
@@ -10,10 +10,10 @@ export default function WhatsappIcon() {
   };
 
   return (
-    <div className="fixed right-3 bottom-3 z-50">
+    <div className="fixed right-3 md:right-5 bottom-20 md:bottom-5 z-50">
       <div
         onClick={handlePesan}
-        className="bg-gradient-to-br from-green-300 to-green-500 dark:from-green-700 dark:to-green-900 text-white space-x-2 flex items-center rounded-full md:p-2 md:px-4 p-2"
+        className="cursor-pointer bg-gradient-to-br from-green-500 via-green-600 to-green-500 text-white space-x-2 flex items-center rounded-full md:p-2 md:px-4 p-2"
       >
         <RiWhatsappFill className="text-white text-3xl" />
         <p className="text-sm hidden md:block">Chat</p>
